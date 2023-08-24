213fe2ba078bc09a6b86e05c31aa20476ab364c1
Sofie2229 <sofia.szachowska@gmail.com>
2023-08-24 09:38:13 +0200

    Initial commit

---
+++ src/headers.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef __CPPGL_HEADERS
+#define __CPPGL_HEADERS
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 // main.cpp
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	if (glfwInit() != GLFW_TRUE) throw std::runtime_error("glfwInit() failed");
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	auto window = glfwCreateWindow(1000, 600, "cppgl", NULL, NULL);
+	
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
